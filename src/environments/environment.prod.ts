15f332db9afa6e98d81c42ae78db0d1c70d19c53
Firas <firasaydi0@gmail.com>
2024-12-29 18:11:30 +0100

    Committing changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBvUkb5bJyBwwMeT64FW8pTU6-0jun7XR8',
+    authDomain: 'wahra-project.firebaseapp.com',
+    projectId: 'wahra-project',
+    storageBucket: 'wahra-project.appspot.com',
+    messagingSenderId: '1050413467626',
+    appId: '1:1050413467626:web:de57338f810a8e89fa5493',
+  },
 };
